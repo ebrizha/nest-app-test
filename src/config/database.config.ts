5f3b39d039d849eb86c6b9f13eefe29b45d86aaa
ebrizha <ebrizha@gmail.com>
2026-02-15 19:25:08 +0200

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,9 +1,9 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('database', () => ({
-  host: process.env.DB_HOST ?? 'localhost',
-  port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USERNAME ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'postgres',
-  name: process.env.DB_NAME ?? 'nest_app_test',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  name: process.env.POSTGRES_DB,
 }));
