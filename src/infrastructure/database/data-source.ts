030bacdc00cfc0406acb25023c050666395c8567
ebrizha <ebrizha@gmail.com>
2026-02-15 19:36:43 +0200

    deploy

--- src/infrastructure/database/data-source.ts
+++ src/infrastructure/database/data-source.ts
@@ -2,13 +2,23 @@ import 'reflect-metadata';
 import { DataSource } from 'typeorm';
 import { ReviewOrmEntity } from './entities/review/review.orm-entity';
 
+
+console.log('!!!!!!!!!!!!!!!!!!!!!!!!!');
+console.log('Data source configuration loaded with values:', {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
+});
+
 const dataSource = new DataSource({
   type: 'postgres',
-  host: process.env.DB_HOST ?? 'localhost',
-  port: Number(process.env.DB_PORT ?? 5432),
-  username: process.env.DB_USERNAME ?? 'postgres',
-  password: process.env.DB_PASSWORD ?? 'postgres',
-  database: process.env.DB_NAME ?? 'nest_app_test',
+  host: process.env.POSTGRES_HOST,
+  port: Number(process.env.POSTGRES_PORT ?? 5432),
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  database: process.env.POSTGRES_DB,
   entities: [ReviewOrmEntity],
   migrations: [__dirname + '/migrations/*{.ts,.js}'],
   synchronize: false,
