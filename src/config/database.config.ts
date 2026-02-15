909a450a001ff5c0b2807384f660921fd46dd997
ebrizha <ebrizha@gmail.com>
2026-02-15 19:32:15 +0200

    deploy

--- src/config/database.config.ts
+++ src/config/database.config.ts
@@ -1,5 +1,14 @@
 import { registerAs } from '@nestjs/config';
 
+console.log('!!!!!!!!!!!!!!!!!!!!!!!!!');
+console.log('Database configuration loaded with values:', {
+  host: process.env.POSTGRES_HOST,
+  port: process.env.POSTGRES_PORT,
+  username: process.env.POSTGRES_USER,
+  password: process.env.POSTGRES_PASSWORD,
+  name: process.env.POSTGRES_DB,
+});
+
 export default registerAs('database', () => ({
   host: process.env.POSTGRES_HOST,
   port: Number(process.env.POSTGRES_PORT ?? 5432),
